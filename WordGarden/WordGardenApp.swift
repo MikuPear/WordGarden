4abfc0fea2fb8564575db5dfb10dbd7cadae8773
David Garcia-Martinez <joshuagarcia_12@yahoo.com>
2026-01-13 11:39:23 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by GARCIA-MARTINEZ, DAVID on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
